14d3b6521af90e5737eb71d0d44a4a3bf4937ccd
Juan Velez <mrjiuby@gmail.con>
2024-05-19 02:33:22 -0500

    Se hace el inicio casi q no

--- myapp/views.py
+++ myapp/views.py
@@ -68,4 +68,4 @@ def registro(request):
 
 @login_required(login_url='login')  # Redirige a 'login' si el usuario no ha iniciado sesión
 def inicio(request):
-    return render(request, 'inicio.html', {'nombre_completo': request.user.nombre_completo})
+    return render(request, 'inicio.html', {'nombre_completo': request.user.nombre_completo, 'correo': request.user.correo})
