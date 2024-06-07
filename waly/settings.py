5238977a5f4d714ac41d4972c3a4a5526c0d53fa
Juan Velez <mrjiuby@gmail.com>
2024-06-06 22:01:18 -0500

    deployment

--- waly/settings.py
+++ waly/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
-ALLOWED_HOSTS = ["waly-e0v1.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,9 +128,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR / 'static')
 ]
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
